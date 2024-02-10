53658ab4135d69e581b389dc2f2ce83454aac21a
Kareem Ayman <Kareem.ayman1622@gmail.com>
2024-02-11 00:10:36 +0200

    Update place_card.dart

--- lib/place_card.dart
+++ lib/place_card.dart
@@ -56,22 +56,22 @@ class _PlaceCardState extends State<PlaceCard> {
               floating: true,
               delegate: _StickyTitleDelegate("The Pyramids Of Giza")),
           SliverFillRemaining(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.w),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ParagraphWidget(
-                        header: "The Eternal Enigma of the Pyramids",
-                        text:
-                            "For millennia, the pyramids of ancient Egypt have stood as monolithic testaments to human ingenuity and ambition. Their geometric precision and sheer scale continue to amaze architects and historians alike, fueling endless debates and hypotheses regarding their construction techniques. The Great Pyramid of Giza, the most iconic of them all, is thought to have been built some 4,500 years ago during the reign of Pharaoh Khufu. This staggering structure, composed of over 2 million massive stone blocks, ascends to a dizzying height of 481 feet, captivating generations with the mysteries of its creation."),
-                    ParagraphWidget(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ParagraphWidget(
+                      header: "The Eternal Enigma of the Pyramids",
+                      text:
+                          "For millennia, the pyramids of ancient Egypt have stood as monolithic testaments to human ingenuity and ambition. Their geometric precision and sheer scale continue to amaze architects and historians alike, fueling endless debates and hypotheses regarding their construction techniques. The Great Pyramid of Giza, the most iconic of them all, is thought to have been built some 4,500 years ago during the reign of Pharaoh Khufu. This staggering structure, composed of over 2 million massive stone blocks, ascends to a dizzying height of 481 feet, captivating generations with the mysteries of its creation."),
+                  Expanded(
+                    child: ParagraphWidget(
                         header: "The Workforce: Myth and Reality",
                         text:
-                            "Contrary to popular depictions, it's widely accepted by Egyptologists that skilled laborers—not slaves—were the cornerstone of the pyramid construction effort. Discoveries of workers' settlements near the pyramids reveal evidence of organized communities with access to food, medical care, and burial chambers. These insights paint a less exploitative picture of their lives.")
-                  ],
-                ),
+                            "Contrary to popular depictions, it's widely accepted by Egyptologists that skilled laborers—not slaves—were the cornerstone of the pyramid construction effort. Discoveries of workers' settlements near the pyramids reveal evidence of organized communities with access to food, medical care, and burial chambers. These insights paint a less exploitative picture of their lives."),
+                  )
+                ],
               ),
             ),
           )
